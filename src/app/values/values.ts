7494e3cef28adb42ca2975630d95d6ff5156d3cd
Sumit Sangwal <sumit0111.dcsa@gmail.com>
2019-08-30 18:09:48 +0530

    some changes in address

--- src/app/values/values.ts
+++ src/app/values/values.ts
@@ -5,7 +5,6 @@ export class Values {
     // public static AUTHERIZATION_TOKEN: string = "autherization_token"
     public static BASE_URL: string = "http://ec2-13-229-228-92.ap-southeast-1.compute.amazonaws.com:3000/api/"
     public static GOOGLE_MAP_URL: string = "https://maps.googleapis.com/maps/api/geocode/json?"
-    // latlng=29.149187,76.717873&key=AIzaSyA3-BQmJVYB6_soLJPv7cx2lFUMAuELlkM"
 
     // public static writeString(key: string, value: string): void {
     //     appSettings.setString(key, value);
